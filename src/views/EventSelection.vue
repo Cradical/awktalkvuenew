0e35406460a86e86dac8b256f74d6322e7933363
Crystal Preston-Watson <32721386+Jadedgeekskipping@users.noreply.github.com>
2018-09-27 09:22:11 -0600

    default buttons for event selection
    Signed-off-by: Crystal Preston-Watson <32721386+Jadedgeekskipping@users.noreply.github.com>

--- src/views/EventSelection.vue
+++ src/views/EventSelection.vue
@@ -1,34 +1,13 @@
 <template>
   <div>
-      <TopNavBar />
-      <BottomMenu />
-    <v-flex xs12 sm6 offset-sm3>
-          <v-layout row wrap>
-            <v-flex
-              v-for="card in cards"
-              v-bind="{ [`xs${card.flex}`]: true }"
-              :key="card.title"
-            >
-              <v-card>
-                <v-img
-                  :src="card.src"
-                >
-                  <v-container
-                    fill-height
-                    fluid
-                    pa-2
-                  >
-                    <v-layout fill-height>
-                      <v-flex xs12 align-end flexbox>
-                        <span class="headline white--text" v-text="card.title"></span>
-                      </v-flex>
-                    </v-layout>
-                  </v-container>
-                </v-img>
-              </v-card>
-            </v-flex>
-          </v-layout>
-    </v-flex>
+    <TopNavBar />
+    <BottomMenu />
+
+    <div class="text-xs-center">
+    <v-btn large outline color="black">Networking</v-btn>
+    <v-btn large outline color="black">Party</v-btn>
+  </div>
+    
   </div>
 </template>
 <script>
@@ -39,13 +18,7 @@ export default {
     components: {
       TopNavBar,
       BottomMenu
-    },
-    data: () => ({
-      cards: [
-        {title: 'Networking', src: 'https://cdn.pixabay.com/photo/2015/01/08/18/27/startup-593341_1280.jpg', flex: 6 },
-        {title: 'Party', src: 'https://cdn.pixabay.com/photo/2015/09/02/13/26/glasses-919071_1280.jpg', flex: 6 }
-      ]
-    })
+    }
 }
 </script>
 <style>
